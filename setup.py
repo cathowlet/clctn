771a13395e64e86535530cbe55d0f8b82a57fe44
mac <mac@macdeMacBook-Pro.local>
2018-05-12 03:22:15 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+setup(name='clctn',
+      version='0.1',
+      description='some collections for being lazy',
+      url='http://github.com/cathowlet/clctn',
+      author='cathowlet',
+      author_email='cathowlet@gmail.com',
+      license='MIT',
+      packages=['clctn','filter'],
+      zip_safe=False)
